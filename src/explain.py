ee4dfa881b413277872b68402ce9150c4f2ae564
zascerta <giammarco.daless@gmail.com>
2023-05-26 17:55:50 +0200

    add args func for parser

--- src/explain.py
+++ src/explain.py
@@ -15,7 +15,7 @@ from utils.datasets import load_dataset, parse_config
 from utils.models import model_selector
 from utils.graphs import normalize_adj
 from utils.plots import plot_graph, plot_expl_loss
-from utils.evaluation import store_expl_log
+from utils.evaluation import store_expl_log, parser_add_args
 
 from evaluations.AUCEvaluation import AUCEvaluation
 from evaluations.EfficiencyEvaluation import EfficiencyEvluation
@@ -24,33 +24,7 @@ CUDA = True
 
 # explainer training
 parser = argparse.ArgumentParser()
-parser.add_argument("--explainer", "-E", type=str, default="CFPG",
-                    choices=["PGEex","CFPG","CFPGv2"])
-parser.add_argument("--dataset", "-D", type=str, default="syn1", 
-                    choices=['syn1','syn2','syn3','syn4'], help="Dataset used for training")
-parser.add_argument("--epochs", "-e", type=int, default=5, help="Number of explainer epochs")
-parser.add_argument("--seed", "-s", type=int, default=42, help="Random seed (default: 42)")
-parser.add_argument("--train-nodes", default=False, action=argparse.BooleanOptionalAction,
-                    help="Whether to explain original train nodes")
-
-# to test gnn conv, may move it to cfg.json
-parser.add_argument("--conv", "-c", type=str, default="GCN", 
-                    choices=["GCN","GAT"], help="Explainer graph convolution ('GCN' or 'GAT')")
-parser.add_argument("--heads", type=int, default=1, help="Attention heads (if conv is 'GAT')")
-parser.add_argument("--add-att", type=float, default=0.0, help="Attention coeff")
-
-# other arguments
-parser.add_argument("--prefix", type=str, default="", 
-                    help="Explainer graph convolution ('GCN' or 'GAT')")
-parser.add_argument("--log", default=False, action=argparse.BooleanOptionalAction, 
-                    help="Whether to store run logs")
-parser.add_argument("--roc", default=False, action=argparse.BooleanOptionalAction, 
-                    help="Whether to plot ROC curve")
-parser.add_argument('--plot-expl', default=False, action=argparse.BooleanOptionalAction, 
-                    help="Plot some of the computed explanation")
-parser.add_argument("--store-adv", default=False, action=argparse.BooleanOptionalAction, 
-                    help="Whether to store adv samples")
-parser.add_argument("--device", "-d", default="cpu", help="Running device, 'cpu' or 'cuda'")
+parser = parser_add_args(parser)
 
 args = parser.parse_args()
 print(">>", args)
@@ -126,8 +100,11 @@ if EXPLAINER == "PGEex":
 elif EXPLAINER == "CFPG":
     explainer = CFPGExplainer(model, graph, epochs=EPOCHS, device=device, coeffs=cfg["expl_params"])
 elif EXPLAINER == "CFPGv2":
-    cfg["expl_params"]["heads"] = args.heads
-    cfg["expl_params"]["add_att"] = args.add_att
+    cfg["expl_params"]["reg_ent"]  = cfg["expl_params"]["reg_ent"] if args.reg_ent == 0.0 else args.reg_ent
+    cfg["expl_params"]["reg_size"] = cfg["expl_params"]["reg_size"] if args.reg_size == 0.0 else args.reg_size
+    cfg["expl_params"]["reg_cf"]   = cfg["expl_params"]["reg_cf"] if args.reg_cf == 0.0 else args.reg_cf
+    cfg["expl_params"]["heads"]    = args.heads
+    cfg["expl_params"]["add_att"]  = args.add_att
     explainer = CFPGv2(model, graph, conv=args.conv, epochs=EPOCHS, coeffs=cfg["expl_params"])
 #elif EXPLAINER == "CF-GNN":
 #    explainer = PCFExplainer(model, graph, norm_adj, epochs=EPOCHS, device=device, coeffs=cfg["expl_params"]) # needs 'CF-GNN' model
--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -1,13 +1,50 @@
 import os
 import torch
+import argparse
 
 from datetime import datetime
 from colorama import init, Fore 
 init(autoreset=True) # initializes Colorama
 
+
+def parser_add_args(parser: argparse.ArgumentParser):
+    """Add arguments to argparser."""
+    parser.add_argument("--explainer", "-E", type=str, default="CFPG",
+                    choices=["PGEex","CFPG","CFPGv2"])
+    parser.add_argument("--dataset", "-D", type=str, default="syn1", 
+                        choices=['syn1','syn2','syn3','syn4'], help="Dataset used for training")
+    parser.add_argument("--epochs", "-e", type=int, default=5, help="Number of explainer epochs")
+    parser.add_argument("--seed", "-s", type=int, default=42, help="Random seed (default: 42)")
+    parser.add_argument("--train-nodes", default=False, action=argparse.BooleanOptionalAction,
+                        help="Whether to explain original train nodes")
+
+    # to test gnn conv, may move it to cfg.json
+    parser.add_argument("--conv", "-c", type=str, default="GCN", 
+                        choices=["GCN","GAT"], help="Explainer graph convolution ('GCN' or 'GAT')")
+    parser.add_argument("--heads", type=int, default=1, help="Attention heads (if conv is 'GAT')")
+    parser.add_argument("--add-att", type=float, default=0.0, help="Attention coeff")
+    parser.add_argument("--reg-ent", type=float, default=0.0, help="Entropy loss coeff")
+    parser.add_argument("--reg-size", type=float, default=0.0, help="Size loss coeff")
+    parser.add_argument("--reg-cf", type=float, default=0.0, help="Pred loss coeff")
+
+    # other arguments
+    parser.add_argument("--prefix", type=str, default="", 
+                        help="Explainer graph convolution ('GCN' or 'GAT')")
+    parser.add_argument("--log", default=False, action=argparse.BooleanOptionalAction, 
+                        help="Whether to store run logs")
+    parser.add_argument("--roc", default=False, action=argparse.BooleanOptionalAction, 
+                        help="Whether to plot ROC curve")
+    parser.add_argument('--plot-expl', default=False, action=argparse.BooleanOptionalAction, 
+                        help="Plot some of the computed explanation")
+    parser.add_argument("--store-adv", default=False, action=argparse.BooleanOptionalAction, 
+                        help="Whether to store adv samples")
+    parser.add_argument("--device", "-d", default="cpu", help="Running device, 'cpu' or 'cuda'")
+
+    return parser
+
+
 path_to_saves = "/../../checkpoints/"
 SAVES_DIR = os.path.dirname(os.path.realpath(__file__)) + path_to_saves
-
 
 def evaluate(out, labels):
     """Calculates the accuracy between the prediction and the ground truth.
