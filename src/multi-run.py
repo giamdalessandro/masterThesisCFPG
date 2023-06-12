81d303924e5c0c3e917c2a34c62c71d61e9fcb30
zascerta <giammarco.daless@gmail.com>
2023-06-12 20:12:51 +0200

    some experiments

--- src/multi-run.py
+++ src/multi-run.py
@@ -1,11 +1,10 @@
 import subprocess
-import  functools
-import  os
+import os
 
 
-DATASETS = ["syn1"] #["syn1","syn2","syn3","syn4"]
+DATASETS = ["syn1","syn2","syn3","syn4"]
 CONVS = ["pGCN","GCN","GAT"]
-EXPLAINER = "PGEex"
+EXPLAINER = "CFPGv2" # "CFPG", "CFPGv2", "PGEex"
 EPOCHS = 20
 
 # returns a list of 8 random small integers between 0 and 255
@@ -19,21 +18,21 @@ def get8RandomBytesFromOS():
 #SIZE_COEFFS = [0.1, 0.01, 0.001]
 #CF_COEFFS = [0.5, 1.0, 2.0, 5.0]
 #NUM_HEADS = [3, 5, 8]
-SEEDS = get8RandomBytesFromOS()
+SEEDS = get8RandomBytesFromOS()[:4]
 
 
 script_cmd  = "/home/zascerta/virtEnvs/XAI-cuda117/bin/python3 src/explain.py "
 rid = 0
-for s in SEEDS:
-    #for h in NUM_HEADS:
-    for d in DATASETS:
-        #for curr in ENT_COEFFS:
-        script_args = f"-E {EXPLAINER} -D {d} -e {EPOCHS} --seed {s} "
-        suffix_args = f"--prefix replPGEex-noNorm-rBest-{EPOCHS}-tests --log"
-        cmd = script_cmd + script_args + suffix_args
+for c in CONVS:
+    for s in SEEDS:
+        for d in DATASETS:
+            #for curr in ENT_COEFFS:
+            script_args = f"-E {EXPLAINER} -D {d} -e {EPOCHS} --conv {c} --seed {s} "
+            suffix_args = f"--prefix replCFPGv2-noDrop-rBest-{EPOCHS} --log"
+            cmd = script_cmd + script_args + suffix_args
 
-        print("\n\n------------------------------ run id:", rid, f"curr-> {EXPLAINER} - {d} - seed {s}\n")
-        returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
-        rid += 1
+            print("\n\n------------------------------ run id:", rid, f"curr-> {EXPLAINER} - {d} - seed {s}\n")
+            returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+            rid += 1
 
 print("\n[runs]> Multi-run DONE...", returned_value)
